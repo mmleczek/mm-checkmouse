29c9622abfa01fb3b6970627faff6cbc3f6f4928
mmleczek <37123369+mmleczek@users.noreply.github.com>
2023-10-20 19:28:09 +0200

    Upload files.

---
+++ main.js
@@ -0,0 +1,108 @@
+const { uIOhook } = require("uiohook-napi");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
+const { getOpenWindows } = require("active-win");
+const activeWindow = require("active-win");
+
+let openWindows = [];
+let selectedWindowId = -1;
+
+let canChangeMovedMouse = true;
+let movedMouse = false;
+let currentTimer = 0;
+let timerState = false;
+
+function secondsToHMS(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+  
+    const hoursStr = String(hours).padStart(2, '0');
+    const minutesStr = String(minutes).padStart(2, '0');
+    const secondsStr = String(remainingSeconds).padStart(2, '0');
+  
+    return `${hoursStr}:${minutesStr}:${secondsStr}`;
+}
+
+uIOhook.on("mousemove", (event) => {
+    if (canChangeMovedMouse) movedMouse = true;
+});
+
+async function createWindow() {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 300,
+        resizable: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.on("resize", () => {
+        const [width, height] = win.getSize();
+        const targetAspectRatio = 400 / 300;
+        const newWidth = Math.max(width, height * targetAspectRatio);
+        const newHeight = newWidth / targetAspectRatio;
+        win.setSize(Math.round(newWidth), Math.round(newHeight));
+    });
+
+    win.loadFile("index.html");
+    win.removeMenu();
+
+    setInterval(async () => {
+        if (movedMouse && timerState) {
+            canChangeMovedMouse = false;
+            movedMouse = false;
+            const curActiveWin = await activeWindow({
+                screenRecordingPermission: false
+            });
+            if (selectedWindowId == curActiveWin?.id) currentTimer = currentTimer + 1;
+            setTimeout(() => canChangeMovedMouse = true, 100);
+            win.webContents.send("timer", {
+                message: secondsToHMS(currentTimer)
+            });
+        }
+    }, 1000);
+
+    openWindows = await getOpenWindows();
+    win.webContents.send("windows", {
+        message: openWindows
+    });
+
+    ipcMain.on("triggerRestart", (event, data) => {
+        console.log("triggerRestart");
+        currentTimer = 0;
+        win.webContents.send("timer", {
+            message: secondsToHMS(currentTimer)
+        });
+    });
+
+    ipcMain.on("timerStatusChange", (event, data) => {
+        timerState = !timerState;
+        win.webContents.send("timerStatus", {
+            message: timerState
+        });
+    });    
+}
+
+ipcMain.on("selectWindow", (event, data) => {
+    selectedWindowId = openWindows[data]?.id;
+});
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+uIOhook.start();
---
+++ preload.js
@@ -0,0 +1,54 @@
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on("timer", (event, data) => {
+    const element = document.getElementById("time")
+    if (element) element.innerText = data.message;
+});
+
+ipcRenderer.on("windows", (event, data) => {
+    for (let i=0; i<data.message.length; i++) {
+        addElemToDropDown("dropdown", i, data.message[i].title);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const startBtn = document.getElementById("start-btn");
+    const restartBtn = document.getElementById("restart-btn");
+
+    ipcRenderer.on("timerStatus", (event, data) => {
+        if (data.message) {
+            startBtn.classList.add("red");
+            startBtn.classList.remove("green");
+            startBtn.innerText = "Stop";
+        } else {
+            startBtn.classList.remove("red");
+            startBtn.classList.add("green");
+            startBtn.innerText = "Start";
+        }
+    });
+    
+    startBtn.onclick = () => {
+        ipcRenderer.send("timerStatusChange", true);
+    }
+    
+    restartBtn.onclick = () => {
+        ipcRenderer.send("triggerRestart", true);
+    }
+});
+
+function addElemToDropDown(id, index, title) {
+    const docelowyElement = document.getElementById(id);
+    const e = document.createElement("a");
+    e.classList.add("dropdown-item");
+    e.textContent = title;
+    e.onclick = () => {
+        selectWindow(index, title);
+    }
+    docelowyElement.appendChild(e);
+}
+
+function selectWindow(index, title) {
+    ipcRenderer.send("selectWindow", index);
+    const dropdownButton = document.querySelector(".btn");
+    dropdownButton.innerText = title;
+}
